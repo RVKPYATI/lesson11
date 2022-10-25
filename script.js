4ff1c26512f3bf561a7558f4cf5df286f30e6942
Vasiliy Rusanov <rvk1986@mail.ru>
2022-10-25 07:23:52 +0300

    Выполнил все задания урока

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+'use strict';
+
 const btn = document.querySelector('#btn');
 const input = document.querySelector('#text');
+const inputRange = document.querySelector('#range');
 const square = document.querySelector('#square');
+const circle = document.querySelector('#circle');
+const eBtn = document.querySelector('#e_btn');
 
 
 function getColor() {
@@ -9,3 +14,10 @@ function getColor() {
 
 
 btn.addEventListener('click', getColor);
+
+eBtn.style.display = 'none';
+
+inputRange.addEventListener('input', function(event) {
+    circle.style.width = event.target.value + '%';
+    circle.style.height = event.target.value + '%';
+});
