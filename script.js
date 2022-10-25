661a4f9ede2af62a8b5ee1a06e8d0414c56380f9
Vasiliy Rusanov <rvk1986@mail.ru>
2022-10-25 07:14:26 +0300

    Выполнил 1 задание

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+const btn = document.querySelector('#btn');
+const input = document.querySelector('#text');
+const square = document.querySelector('#square');
 
 
+function getColor() {
+    square.style.background = input.value;
+}
 
 
-
+btn.addEventListener('click', getColor);
