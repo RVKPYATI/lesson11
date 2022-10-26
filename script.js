ae7c972639472e231b773b380617b5ee92e6ad6f
Vasiliy Rusanov <rvk1986@mail.ru>
2022-10-26 18:22:33 +0300

    Переделал задание

--- script.js
+++ script.js
@@ -6,18 +6,23 @@ const inputRange = document.querySelector('#range');
 const square = document.querySelector('#square');
 const circle = document.querySelector('#circle');
 const eBtn = document.querySelector('#e_btn');
+const percentCount = document.querySelector('#range-span');
 
 
 function getColor() {
     square.style.background = input.value;
+    input.value = '';
 }
 
-
-btn.addEventListener('click', getColor);
-
+function circleSize() {
+    circle.style.width = inputRange.value + '%';
+    circle.style.height = inputRange.value + '%';
+    percentCount.textContent = inputRange.value + '%';
+}
 eBtn.style.display = 'none';
 
-inputRange.addEventListener('input', function(event) {
-    circle.style.width = event.target.value + '%';
-    circle.style.height = event.target.value + '%';
-});
+circleSize();
+btn.addEventListener('click', getColor);
+inputRange.addEventListener('input', circleSize);
+
+
